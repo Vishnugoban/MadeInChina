f3755882fa4a7e36b620fa590e96762b308edbb5
Vishnugoban Anandharajah <kvgoban@gmail.com>
2025-05-27 20:32:45 +0530

    working on personald etails

--- src/screens/Account/PersonalDetails.jsx
+++ src/screens/Account/PersonalDetails.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -7,6 +7,7 @@ import { useNavigation } from "@react-navigation/native";
 import BackBtnCus from "../../components/BackBtnCus";
 import TextBoxCus from "../../components/TextBoxCus";
 import BtnCus from "../../components/BtnCus";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const PersonalDetails = () => {
   const navigation = useNavigation();
@@ -15,23 +16,36 @@ const PersonalDetails = () => {
     <SafeAreaView style={styles.container}>
       <View style={{ width: "100%" }}>
         <BackBtnCus />
-        <Text style={[styles.title, { marginTop: 15 }]}>Reset Password</Text>
+        <Text style={[styles.title, { marginTop: 15 }]}>Personal Details</Text>
       </View>
-      <Text style={styles.subTitle}>
-        Enter your new password to complete the reset process
-      </Text>
 
+      <View style={styles.profileSection}>
+        <View>
+          <Image
+            source={{
+              uri: "https://www.w3schools.com/howto/img_avatar.png",
+            }}
+            style={styles.profileImage}
+          />
+          <View style={styles.editIcon}>
+            <MaterialIcons name="edit" size={18} color="#fff" />
+          </View>
+        </View>
+      </View>
+
+      {/* Input Fields */}
       <View style={{ width: "100%" }}>
+        <TextBoxCus label="Name" placeholder="Zara Zynna" />
+        <TextBoxCus label="Email" placeholder="zara@gmail.com" />
+        <TextBoxCus label="Phone Number" placeholder="+94 76 123 4567" />
         <TextBoxCus
-          label="New Password"
-          placeholder="must be 8 characters"
+          label="Password"
+          placeholder="********"
           secureTextEntry={true}
         />
-        <TextBoxCus
-          label="Confirm New Password"
-          placeholder="repeat password"
-          secureTextEntry={true}
-        />
+        <TouchableOpacity onPress={() => navigation.navigate("ResetPwd")}>
+          <Text style={styles.changePwdText}>Forgot password?</Text>
+        </TouchableOpacity>
       </View>
 
       {/* Spacer */}
@@ -47,7 +61,7 @@ const PersonalDetails = () => {
               onPressType: "navigateToLogin",
             })
           }
-          text="Reset Password"
+          text="Save Changes"
           google={false}
         />
       </View>
@@ -73,12 +87,36 @@ const styles = StyleSheet.create({
     marginBottom: "3%",
   },
 
-  subTitle: {
+  changePwdText: {
     fontFamily: "Roboto",
-    fontSize: 16,
-    color: "#000",
-    fontWeight: "400",
-    lineHeight: 24,
-    marginTop: 15,
+    fontSize: 12,
+    color: "#F54135",
+    fontWeight: "500",
+    textDecorationLine: "underline",
+    textDecorationStyle: "solid",
+    textAlign: "right",
+    marginTop: 10,
+  },
+
+  profileSection: {
+    alignItems: "center",
+    marginVertical: 10,
+  },
+
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+
+  editIcon: {
+    position: "absolute",
+    bottom: 0,
+    right: -5,
+    backgroundColor: "#4392F9",
+    padding: 6,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: "#fff",
   },
 });
