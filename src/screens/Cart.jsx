209b08299b905ea7a54c66d72f5811e4b1c3d407
Vishnugoban Anandharajah <kvgoban@gmail.com>
2025-05-28 13:16:05 +0530

    working on Cart ui

--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Text, StyleSheet, ScrollView } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
 
 const Cart = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.header}>My Cart</Text>
+    </ScrollView>
   );
 };
 
-export default Cart;
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    flexGrow: 1,
+  },
 
-const styles = StyleSheet.create({});
+  header: {
+    fontFamily: "Roboto",
+    fontSize: 24,
+    fontWeight: "600",
+    alignSelf: "center",
+    marginBottom: 30,
+    color: "#4D3E3E",
+  },
+});
+
+export default Cart;
